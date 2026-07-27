9fc044593d7d0e0fdcdc17735c7bc2b5121489a7
S0Ale <pelizzoni_sale@protonmail.com>
2026-07-27 17:41:25 +0200

    Ethereum: fixed bug in the account change script

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
-import { loginGate, connect } from './login-module.js'
+import { returnToLogin } from './login-module.js'
 
-async function init() {
+function init(){
 	if (!window.ethereum) return;
 
 	// listen for account change/disconnect events
 	window.ethereum.on("accountsChanged", async (accounts) => {
-		window.location.href = '/'
+		returnToLogin()
 	});
-}()
+}
+init()
